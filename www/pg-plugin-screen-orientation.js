b599a02ac019a695472dd5c439e7b3aff577546d
Apon Palanuwech <palaa159@newschool.edu>
2013-10-12 02:38:41 -0400

    Update pg-plugin-screen-orientation.js

--- www/pg-plugin-screen-orientation.js
+++ www/pg-plugin-screen-orientation.js
@@ -1,6 +1,13 @@
-var screenOrientation = function() {}
-
-screenOrientation.prototype.set = function(str, success, fail) {
-	cordova.exec(null, null, "ScreenOrientation", "set", [str]);
-};
-navigator.screenOrientation = new screenOrientation();
+cordova.define("cordova/plugin/screenorientation", function(require, exports, module) {
+    var exec = require('cordova/exec');    
+    var ScreenOrientation = function() {};    
+    ScreenOrientation.prototype.set = function(str, successCallback, errorCallback) {
+        exec(successCallback,
+    errorCallback,
+    'ScreenOrientation',
+    'set',
+    [str]);
+    };        
+    var screenorientation = new ScreenOrientation();
+    module.exports = screenorientation;
+});
